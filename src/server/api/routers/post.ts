aa63298c64bef9ec72db9f2055de2c69c6996b79
jerome jesared <jesared@gmail.com>
2026-03-20 14:45:22 +0100

    up

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -16,12 +16,19 @@ export const postRouter = createTRPCRouter({
     }),
 
   create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
+    .input(
+      z.object({
+        title: z.string().min(1),
+        content: z.string().min(1),
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
       return ctx.db.post.create({
         data: {
-          name: input.name,
-          createdBy: { connect: { id: ctx.session.user.id } },
+          title: input.title,
+          content: input.content,
+          slug: input.title.toLowerCase().replace(/\s+/g, "-"),
+          authorId: ctx.session.user.id,
         },
       });
     }),
@@ -29,7 +36,7 @@ export const postRouter = createTRPCRouter({
   getLatest: protectedProcedure.query(async ({ ctx }) => {
     const post = await ctx.db.post.findFirst({
       orderBy: { createdAt: "desc" },
-      where: { createdBy: { id: ctx.session.user.id } },
+      where: { authorId: ctx.session.user.id },
     });
 
     return post ?? null;
